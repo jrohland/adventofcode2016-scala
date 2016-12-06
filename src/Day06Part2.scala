ecf901caebcb90064f60060102354d85ec3dacd2
Jesse Rohland <jesse.rohland@gmail.com>
2016-12-06 16:32:17 -0500

    Day 6 solution

---
+++ src/Day06Part2.scala
@@ -0,0 +1,24 @@
+/**
+  * Created by jesse on 12/6/2016.
+  */
+object Day06Part2 {
+
+  def main(args: Array[String]): Unit = {
+
+    val input = scala.io.Source.fromFile("input/day06-input.txt").getLines()
+
+    val codeMap = input.flatMap(line => {
+      (0 until line.length).map(i => {
+        i -> line.charAt(i)
+      })
+    }).toList.groupBy(_._1).mapValues(chars => {
+      chars.map(_._2).groupBy(identity).mapValues(_.size).toSeq.sortBy(_._2).head._1
+    })
+
+    val code = (0 until codeMap.size).map(codeMap(_)).mkString
+
+    println(code)
+
+  }
+
+}
